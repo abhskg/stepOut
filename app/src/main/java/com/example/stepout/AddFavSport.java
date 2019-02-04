120e16b073471764064fef0c990786a4a21a58e9
abhskg <abhasgorain@gmail.com>
2019-02-04 19:12:07 +0530

    Temp Add profile and fav sport

---
+++ app/src/main/java/com/example/stepout/AddFavSport.java
@@ -0,0 +1,14 @@
+package com.example.stepout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFavSport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_fav_sport);
+    }
+}
